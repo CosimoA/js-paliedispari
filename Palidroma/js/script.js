39f03a08b125cfe5ce8e1ac336fec41a3587f88d
CosimoA <cosimo.archip@gmail.com>
2023-11-27 19:15:39 +0100

    FEAT: add function reverse word & output

--- Palidroma/js/script.js
+++ Palidroma/js/script.js
@@ -1,11 +1,37 @@
-/* Palidroma:
+/* 
+Palidroma:
 Chiedere all’utente di inserire una parola.
 Creare una funzione per capire se la parola inserita è palindroma. */
 
 // Variabili Preimpostate
+let parolaScelta = "";
+let parolaAlContrario = "";
 
 // Inserisci una parola
+parolaScelta = prompt("Inserisci una parola in minuscolo");
+console.log("La parola scelta è: " + parolaScelta);
 
 // crea funzione per invertire la parola
+function invertiParola(parolaScelta){
+    let parolaAlContrario = '';
+    let i = parolaScelta.length - 1;
+    while (i >= 0) {
+        parolaAlContrario += parolaScelta[i];
+        i--;
+    }
+  
+    return parolaAlContrario;
+  }
+
+// definisco la parola al contrario
+
+parolaAlContrario = invertiParola(parolaScelta);
+console.log("La parola al contrario è: " + parolaAlContrario);
 
 // output Risultato
+
+if(parolaScelta === parolaAlContrario){
+    console.log("La parola è palindroma!");
+  } else {
+    console.log("La parola non è palindroma!");
+}
