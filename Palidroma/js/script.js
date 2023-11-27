9ebe7fe23d0f4f3510db2fffa6d3dbe52750abaf
CosimoA <cosimo.archip@gmail.com>
2023-11-27 18:50:02 +0100

    CONFIG: base scaffolding Palindroma & define flux

---
+++ Palidroma/js/script.js
@@ -0,0 +1,11 @@
+/* Palidroma:
+Chiedere all’utente di inserire una parola.
+Creare una funzione per capire se la parola inserita è palindroma. */
+
+// Variabili Preimpostate
+
+// Inserisci una parola
+
+// crea funzione per invertire la parola
+
+// output Risultato
